904b3c7b10449d441b243130fb558a19c1664fd2
Kush <kushthaker@gmail.com>
2015-07-29 10:47:33 -0400

    registration issue resolved, login working

--- frontend/js/controllers/landingCtrl.js
+++ frontend/js/controllers/landingCtrl.js
@@ -1,5 +1,14 @@
-function LandingCtrl() {
-
+function LandingCtrl($location, $auth, user) {
+	this.location = $location;
+	this.auth = $auth;
+	this.user = user;
+	this.loginForm = {};
 }
 
 angular.module('setUpShopApp').controller('landingCtrl', LandingCtrl);
+
+LandingCtrl.prototype.login = function() {
+	this.user.loginForm = this.loginForm;
+	this.user.login();
+	this.location.path('/landing');
+};
--- setupshopapp/app/models/user.rb
+++ setupshopapp/app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :omniauthable
   include DeviseTokenAuth::Concerns::User
 end
