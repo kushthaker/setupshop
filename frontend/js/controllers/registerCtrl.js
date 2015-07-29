3eeb1008dd6f43d2d863d148f3c79d830bc7604e
Kush <kushthaker@gmail.com>
2015-07-28 20:08:35 -0400

    500 error in user registration setup

--- frontend/js/controllers/registerCtrl.js
+++ frontend/js/controllers/registerCtrl.js
@@ -1,5 +1,16 @@
-function RegisterCtrl() {
+function RegisterCtrl($location, $auth, user) {
+
+	this.location = $location;
+	this.auth = $auth;
+	this.user = user;
+	this.registrationForm = {};
 
 }
 
 angular.module('setUpShopApp').controller('registerCtrl', RegisterCtrl);
+
+RegisterCtrl.prototype.register = function() {
+	this.user.registrationForm = this.registrationForm;
+	this.user.register();
+	this.location.path('/landing');
+};
--- frontend/js/services/userService.js
+++ frontend/js/services/userService.js
@@ -1,5 +1,31 @@
-function UserService() {
+function UserService($auth) {
+
+	this.auth = $auth;
+	this.registrationForm = {};
+	this.loginForm = {};
 
 }
 
 angular.module('setUpShopApp').service('user', UserService);
+
+UserService.prototype.login = function() {
+	var self = this;
+	self.auth.submitLogin(self.loginForm)
+	.then(function(resp) {
+		console.log('successful login', resp);
+		})
+	.catch(function(resp) {
+		console.log('unsuccessful login', resp);
+	});
+};
+
+UserService.prototype.register = function() {
+	var self = this;
+	self.auth.submitRegistration(self.registrationForm)
+	.then(function(resp) {
+		console.log('successful registration', resp);
+      })
+	.catch(function(resp) {
+        console.log('unsuccessful registration', resp);
+      });
+};
